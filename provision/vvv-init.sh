6053960994d319906dbfef28a10e885e0d3cb3dc
Jonas Tampier <jonas@tampier.de>
2019-02-12 16:46:20 +0100

    added woocommerce plugin

--- provision/vvv-init.sh
+++ provision/vvv-init.sh
@@ -57,6 +57,10 @@ if ! $(noroot wp core is-installed); then
   fi
 
   noroot wp core ${INSTALL_COMMAND} --url="${DOMAIN}" --quiet --title="${SITE_TITLE}" --admin_name=admin --admin_email="admin@local.test" --admin_password="password"
+  
+  echo "Installing plugins"
+  noroot wp plugin install woocommerce --activate 
+  
 else
   echo "Updating WordPress Stable..."
   cd ${VVV_PATH_TO_SITE}/public_html
